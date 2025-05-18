343f55189c1b067de0b7d7b581a67003cd501730
SMIT-1803 <smitsachin1@gmail.com>
2025-05-18 12:27:07 -0700

    Fixed typo bug

--- src/store/store.js
+++ src/store/store.js
@@ -2,8 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import authSlice from './authSlice';
 
 const store = configureStore({
-    reducers:{
-        auth:authSlice
+    reducer:{
+        auth:authSlice,
     }
 })
 
