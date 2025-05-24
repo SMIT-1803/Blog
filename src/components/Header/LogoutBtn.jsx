f3f3241ffe44216bdd4a688974fbd45a1da2da68
SMIT-1803 <smitsachin1@gmail.com>
2025-05-24 16:55:58 -0700

    Redirecting user to Login on clicking logout button

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import authService from "../../appwrite/auth";
 import { logout } from "../../store/authSlice";
-
+import { useNavigate } from "react-router-dom";
 function LogoutBtn() {
   const dispatch = useDispatch();
+  const navigate = useNavigate()
   const logoutHandler = () => {
     authService.logout().then(dispatch(logout()));
+    navigate("/login")
   };
   return (
     <button onClick={logoutHandler} className="inline-block px- py-2 duration-200 hover:bg-blue-100 rounded-full">
