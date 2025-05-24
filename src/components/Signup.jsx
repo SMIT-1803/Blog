c6f2f2e827663fa435015e8151af959ae5161bd6
SMIT-1803 <smitsachin1@gmail.com>
2025-05-24 16:41:32 -0700

    Fixed information dispatch error

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -18,7 +18,7 @@ function Signup() {
       const createAccount = await authService.createAccount(data);
       if (createAccount) {
         const userData = await authService.getCurrentUser();
-        if (userData) dispatch(login(userData));
+        if (userData) dispatch(login({userData:userData}));
         navigate("/");
       }
     } catch (error) {
